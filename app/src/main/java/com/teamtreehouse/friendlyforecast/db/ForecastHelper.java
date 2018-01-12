c071d7368e8952c46714a7839d37bcf6f92f2901
bobtal <talevski.boban@gmail.com>
2018-01-12 01:25:11 +0100

    Adds a new database version along with the proper onUpgrade implementation and update of the SQL to be executed in onCreate

--- app/src/main/java/com/teamtreehouse/friendlyforecast/db/ForecastHelper.java
+++ app/src/main/java/com/teamtreehouse/friendlyforecast/db/ForecastHelper.java
@@ -9,13 +9,18 @@ public class ForecastHelper extends SQLiteOpenHelper {
     public static final String TABLE_TEMPERATURES = "TEMPERATURES";
     public static final String COLUMN_ID = "_ID";
     public static final String COLUMN_TEMPERATURE = "TEMPERATURES";
+    public static final String COLUMN_TIME = "TIME";
 
     private static final String DB_NAME = "temperatures.db";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2; // Must increment to trigger an upgrade
     private static final String DB_CREATE =
             "CREATE TABLE " + TABLE_TEMPERATURES + " (" +
                     COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    COLUMN_TEMPERATURE + " REAL)";
+                    COLUMN_TEMPERATURE + " REAL, " +
+                    COLUMN_TIME + " INTEGER)";
+
+    private static final String DB_ALTER =
+            "ALTER TABLE " + TABLE_TEMPERATURES + " ADD " + COLUMN_TIME + " INTEGER";
 
     public ForecastHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -28,6 +33,6 @@ public class ForecastHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(DB_ALTER);
     }
 }
